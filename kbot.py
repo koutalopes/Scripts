62581f46371d438f0c6d2df9f967b013d874d6d6
koutalopes <rykrdo.lopes@gmail.com>
2017-01-30 22:40:02 -0200

    Update Files

--- kbot.py
+++ kbot.py
@@ -8,13 +8,14 @@
 
 import socket
 import ssl
+import datetime
 import time
 
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sirc = ssl.wrap_socket(ircsock)
+sirc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sirc = ssl.wrap_socket(ircsock)
 
 server = "irc.rizon.net"
-port = 9999 # Secure connection
+port = 6667 # Secure connection
 channel = "#flooders"
 botnick = "kBot"
 adminname = "Kouta_Kun"
@@ -24,28 +25,29 @@ con = False
 def connect():
 	try:
 		sirc.connect((server, port))
-		sirc.send(bytes("USER " + botnick + " " + botnick + " " + botnick + " Python Bot Test\n", "UTF-8"))
-		sirc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
+		sirc.send(bytes("USER {} {} {} Python Bot Test\n".format(botnick, botnick, botnick), "UTF-8"))
+		sirc.send(bytes("NICK {}\n".format(botnick), "UTF-8"))
 		con = True
 	except Exception:
 		con = False
 
 def pong(ping):
 	pingn = ping.split()[1]
-	sirc.send(bytes("PONG " + pingn + "\n", "UTF-8"))
-	print ("PONG " + pingn)
+	sirc.send(bytes("PONG {}\r\n".format(pingn), "UTF-8"))
+	print ("PONG {}".format(pingn))
 
 def sendmsg(msg, target=channel):
-	sirc.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
+	sirc.send(bytes("PRIVMSG {} :{}\n".format(target, msg), "UTF-8"))
 
 def main():
 	while 1:
 		ircmsg = sirc.recv(2048).decode("UTF-8")
 		ircmsg = ircmsg.strip('\n\r')
-		print(ircmsg)
+		st = str(datetime.datetime.now()).split('.')[0]
+		print ('[{}] {}'.format(st, ircmsg))
 
 		if ircmsg.find("End of /MOTD command.") != -1:
-			sirc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
+			sirc.send(bytes("JOIN {}\n".format(channel), "UTF-8"))
 
 		if ircmsg.find("PRIVMSG") != -1:
 			nick = ircmsg.split('!',1)[0][1:]
@@ -60,11 +62,12 @@ def main():
 					return
 
 				if msg.split()[0] == "!join":
-					sirc.send(bytes("JOIN " + msg.split()[1] + "\n", "UTF-8"))
-					print("JOIN " + msg.split()[1])
+					sirc.send(bytes("JOIN {}\n".format(msg.split()[1]), "UTF-8"))
+					print("JOIN {}".format(msg.split()[1]))
 
 		else:
 			if ircmsg.find("PING :") != -1:
+				time.sleep(2)
 				pong(ircmsg)
 
 connect()
