a6f467809281b2f9f508bc9458efe1e1dc55965e
koutalopes <koutalopes@users.noreply.github.com>
2019-01-06 17:26:20 -0200

    Delete wall_changer.sh

--- wall_changer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-arq=$(shuf -n 1 'caminho para o aquivo com a lista' | sed 's/ /\%20/g')
-
-gsettings set org.gnome.desktop.background picture-uri file:///'caminho para a pasta de wallpapers'/$arq
-gsettings set org.gnome.desktop.background picture-options 'scaled'
-gsettings set org.gnome.desktop.background primary-color '#716b6b'
-gsettings set org.gnome.desktop.background secondary-color '#7d7e4f'
-gsettings set org.gnome.desktop.background color-shading-type 'vertical'
