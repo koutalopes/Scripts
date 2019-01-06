77aa297178fe674b12c7aa9be569cf4ee1002a02
koutalopes <koutalopes@users.noreply.github.com>
2019-01-06 17:23:40 -0200

    Update wall_changer.sh

--- wall_changer.sh
+++ wall_changer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-arq=$(shuf -n 1 ~/Pictures/Wallpapers/list | sed 's/ /\%20/g')
+arq=$(shuf -n 1 'caminho para o aquivo com a lista' | sed 's/ /\%20/g')
 
-gsettings set org.gnome.desktop.background picture-uri file:///home/kouta/Pictures/Wallpapers/$arq
+gsettings set org.gnome.desktop.background picture-uri file:///'caminho para a pasta de wallpapers'/$arq
 gsettings set org.gnome.desktop.background picture-options 'scaled'
 gsettings set org.gnome.desktop.background primary-color '#716b6b'
 gsettings set org.gnome.desktop.background secondary-color '#7d7e4f'
